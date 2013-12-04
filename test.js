2826ea8a2b64992870e99ad9a75f8c2f233cd32c
Martin Majer <martin@martinmajer.net>
2013-12-04 17:11:48 +0100

    Basic compiler, unfinished.

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 "use strict";
 
 var cutter = require("./lib/cutter.js");
-cutter.get("./test/test1.jtpl", function(err, template) {
+
+var output = "";
+
+function outputCallback(snippet, text) {
+    if (snippet === null) output += text;
+    else {
+        /*if (snippets[snippet] == undefined) snippets[snippet] = "";
+        snippets[snippet] += text;*/
+    }
+}
+
+cutter.get("./test/parent.jtpl", function(err, template) {
     // do something with the template
-    
+    cutter.run(template, { title: "Hello world", }, null, outputCallback);
+    console.log(output);
 });
+
 

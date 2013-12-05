65c928da7f3220ecb87063d8f76afe73a597aa27
Martin Majer <martin@martinmajer.net>
2013-12-06 00:19:08 +0100

    Support for if, else, while and for. Better indentation and newlines handling.

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ function outputCallback(snippet, text) {
 
 cutter.get("./test/parent.jtpl", function(err, template) {
     // do something with the template
-    cutter.run(template, { title: "Hello world", }, null, outputCallback);
+    cutter.run(template, { title: "Hello world", a: true, b: true, array: [1, 2, 3, 4, 5] }, null, outputCallback);
     console.log(output);
 });
 

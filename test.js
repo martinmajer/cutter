851ef0422d22a22adf44b0858a7488678adbc939
Martin Majer <martin@martinmajer.net>
2013-12-06 22:47:59 +0100

    Proper exception handling.

--- test.js
+++ test.js
@@ -14,6 +14,10 @@ function outputCallback(snippet, text) {
 }
 
 cutter.get("./test/child2.jtpl", function(err, template) {
+    if (err) {
+        console.log(err.message + " at " + err.filename + ":" + err.line);
+        return;
+    }
     // do something with the template
     cutter.run(template, { title: "Hello world", a: true, b: true, array: [1, 2, 3, 4, 5] }, null, outputCallback);
     if (output) console.log(output);

67f68e9a8e384948c266cb9f3e1973a31a4b86ff
Martin Majer <martin@martinmajer.net>
2013-12-06 20:53:50 +0100

    Support for template cutouts.

--- test.js
+++ test.js
@@ -3,19 +3,21 @@
 var cutter = require("./lib/cutter.js");
 
 var output = "";
+var snippets = {};
 
 function outputCallback(snippet, text) {
     if (snippet === null) output += text;
     else {
-        /*if (snippets[snippet] == undefined) snippets[snippet] = "";
-        snippets[snippet] += text;*/
+        if (snippets[snippet] == undefined) snippets[snippet] = "";
+        snippets[snippet] += text;
     }
 }
 
 cutter.get("./test/child2.jtpl", function(err, template) {
     // do something with the template
     cutter.run(template, { title: "Hello world", a: true, b: true, array: [1, 2, 3, 4, 5] }, null, outputCallback);
-    console.log(output);
+    if (output) console.log(output);
+    if (Object.keys(snippets).length) console.log(snippets);
 });
 
 

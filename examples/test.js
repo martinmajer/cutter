eb064ab83b50e746c8979044fc6eedaf8e841ef0
Martin Majer <martin@martinmajer.net>
2013-12-11 22:56:50 +0100

    Examples.

---
+++ examples/test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var cutter = require("..");
+
+var templateFile = process.argv[2];
+if (!templateFile) {
+    console.log("Please specify the example template!");
+    return;
+}
+
+var data = {
+    hello: "world",
+    html:  "<p>The quick brown fox jumps over the lazy dog</p>",
+    gizmo: "g.i.z.m.o",
+    array: [1, 11, 111, 1111, 11111],
+};
+cutter.get(templateFile, function(err, template) {
+    if (err) {
+        console.log(err.message + " at " + err.filename + ":" + err.line);
+        return;
+    }
+    console.log(template.fetch(data));
+});
+
